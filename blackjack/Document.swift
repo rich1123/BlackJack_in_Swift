a036c0d3f2da38567e8ac35163d85a65bbea16a0
Rich Maiale <richm1123@gmail.com>
2020-04-29 19:20:47 -0400

    Initial Commit

---
+++ blackjack/Document.swift
@@ -0,0 +1,22 @@
+//
+//  Document.swift
+//  blackjack
+//
+//  Created by rich on 4/29/20.
+//  Copyright © 2020 rich. All rights reserved.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
---
+++ blackjack/DocumentView.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentView.swift
+//  blackjack
+//
+//  Created by rich on 4/29/20.
+//  Copyright © 2020 rich. All rights reserved.
+//
+
+import SwiftUI
+
+struct DocumentView: View {
+    var document: UIDocument
+    var dismiss: () -> Void
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text("File Name")
+                    .foregroundColor(.secondary)
+
+                Text(document.fileURL.lastPathComponent)
+            }
+
+            Button("Done", action: dismiss)
+        }
+    }
+}
